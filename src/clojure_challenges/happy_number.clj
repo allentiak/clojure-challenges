6f759f5ba8cc04a88aaff9cba8d1301bfc07a9c5
Leandro Doctors <ldoctors@gmail.com>
2018-05-11 21:52:31 -0300

    Happy number: almost there...
    java.lang.StackOverflowError:

--- src/clojure_challenges/happy_number.clj
+++ src/clojure_challenges/happy_number.clj
@@ -2,7 +2,23 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-(reduce + (filter pos? (map (comp read-string str) "130")))
+(defn process
+  [n]
+  (reduce + (map #(* % %) (filter pos? (map (comp read-string str) (str n))))))
+
+(defn- happy?-aux
+  [n history]
+  (let [result (process n)
+        new-history (into history '(result))]
+    (if (= result 1) true
+           (if (contains? history result) false
+               (happy?-aux result new-history)))))
+
+(defn happy?
+  [n]
+  (let [result (process n)]
+    (if (= result 1) true
+      (happy?-aux result #{result}))))
 
 (defn -main
   "Happy number challenge."
