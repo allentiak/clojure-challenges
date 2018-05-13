ec01b175abc0f12840877ff6e8b6eb243fd51979
Leandro Doctors <ldoctors@gmail.com>
2018-05-12 23:00:16 -0300

    Happy number: add new test (with 'are')

--- test/clojure_challenges/happy_number_test.clj
+++ test/clojure_challenges/happy_number_test.clj
@@ -2,8 +2,8 @@
   (:require [clojure.test :refer :all]
             [clojure-challenges.happy-number :refer :all]))
 
-(deftest happy?-test
-  (testing "'happy?' fn."
+(deftest happy?-is-test
+  (testing "'happy?' fn (with 'is)."
     (is (= (happy? 1) true))
     (is (= (happy? 10) true))
     (is (= (happy? 100000) true))
@@ -11,3 +11,14 @@
     (is (= (happy? 986543210) true))
     (is (= (happy? 3) false))
     (is (= (happy? 2) false))))
+
+(deftest happy?-are-test
+  (testing "'happy' fn (with 'are')."
+    (are [n truth-value] (= (happy? n) truth-value)
+      1 true
+      10 true
+      100000 true
+      7 true
+      986543210 true
+      3 false
+      2 false)))
