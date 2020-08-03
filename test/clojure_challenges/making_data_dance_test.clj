64ca337281ef18db92962c5ddcad729c0630c162
Leandro Doctors <ldoctors@gmail.com>
2020-08-03 16:34:00 -0300

    making data dance: fix tests

--- test/clojure_challenges/making_data_dance_test.clj
+++ test/clojure_challenges/making_data_dance_test.clj
@@ -7,8 +7,8 @@
   (testing "dance fn"
     (is (= "1, 2, 3" (str (dance 2 1 3))))
     (is (= '(2 1 3) (seq (dance 2 1 3))))
-    ;;(is (= '(2 1 3) (seq (dance 2 1 3 3 1 2)) true))
-    ;;(is (= '(1) (seq (apply dance (repeat 5 1))) true))
-    ;;(is (= "1, 1, 1, 1, 1" (str (apply dance (repeat 5 1))) true))
+    (is (= '(2 1 3) (seq (dance 2 1 3 3 1 2))))
+    (is (= '(1) (seq (apply dance (repeat 5 1)))))
+    (is (= "1, 1, 1, 1, 1" (str (apply dance (repeat 5 1)))))
     (is (and (= nil (seq (dance)))
              (=  "" (str (dance)))))))
