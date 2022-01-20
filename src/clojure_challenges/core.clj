6af0148d1447c1d6acbc6b67785626a3ea0c7910
Leandro Doctors <ldoctors@gmail.com>
2022-01-20 19:22:57 -0300

    project: delete 'core' namespace

--- src/clojure_challenges/core.clj
+++
@@ -1,5 +0,0 @@
-(ns clojure-challenges.core
-  (:gen-class))
-
-(defn -main [& args]
-  (prn 'ok))
--- test/clojure_challenges/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-challenges.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-challenges.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I always pass."
-    (is (= 1 1))))
