44db7ab63cb5e700d2b3007c891eadc28ddf5c9a
Leandro Doctors <ldoctors@gmail.com>
2022-02-03 19:23:49 -0300

    spike: start playing with liberator (WIP)

--- src/clojure_challenges/server/scramble_webserver.clj
+++ src/clojure_challenges/server/scramble_webserver.clj
@@ -1,22 +1,45 @@
 (ns clojure-challenges.server.scramble-webserver
   (:require
+   [liberator.core :refer [defresource]]
+   [liberator.dev :refer [wrap-trace]]
    [clojure-challenges.apis.scramble :as scramble]
-   [compojure.core :refer [defroutes GET POST]]
+   [compojure.core :refer [defroutes ANY GET POST]]
    [compojure.route :as route]
    [ring.adapter.jetty :as jetty]
    [ring.middleware.defaults :refer [api-defaults wrap-defaults]]
    [ring.middleware.reload :refer [wrap-reload]]))
 
-(defn default-page []
+;; This fn *has* to be public. The param *has* to be there, even if it is never used. It is passed from the resource by Liberator.
+(defn default-page [param]
   "Just a RESTful interface for 'scramble?'")
+
+(defn- default-page-with-param [param]
+  (str "Just a RESTful interface for 'scramble?'
+  This is the param: " param))
 
 (defn- scramble-handler
   [letters word]
   (str (scramble/scramble? letters word)))
 
+(defresource scramble-resource
+  [letters word]
+  :available-media-types ["text/plain"]
+  :allowed-methods [:post]
+  :handle-ok (fn [] scramble-handler letters word))
+
+(defresource default-page-resource
+  :available-media-types ["text/plain"]
+  :handle-ok default-page)
+
+(defresource default-page-resource-with-param
+  [param]
+  :available-media-types ["text/plain"]
+  :handle-ok (fn [_] (default-page-with-param param)))
+
 (defroutes webserver-routes
-  (GET "/" [] (default-page))
-  (POST "/scramble" [letters word] (scramble-handler letters word))
+  (GET "/" [] default-page-resource)
+  (GET "/:param" [param] (default-page-resource-with-param param))
+  ;;(POST "/scramble" [letters word] (scramble-resource letters word))
   (route/not-found "This isn't the page you're looking for."))
 
 (def webserver
@@ -24,7 +47,8 @@
       (wrap-defaults api-defaults)))
 
 (def dev-webserver
-  (-> #'webserver
+  (-> webserver
+      (wrap-trace :header :ui)
       (wrap-reload)))
 
 ;; it won't work unless I use the dash
